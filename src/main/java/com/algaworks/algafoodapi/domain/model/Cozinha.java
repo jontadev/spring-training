fd0d0067b2b86420c0dd493bb7972f39ee140fdc
jontadev <jontadev@gmail.com>
2021-09-29 00:34:25 +0200

    Hikari connection pool configuration and start with migration database with Flyway

--- src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafoodapi/domain/model/Cozinha.java
@@ -1,18 +1,22 @@
 package com.algaworks.algafoodapi.domain.model;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
-@Getter
-@Setter
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 @Table(name = "cozinha")
@@ -29,5 +33,4 @@ public class Cozinha {
     @JsonIgnore
     @OneToMany(mappedBy = "cozinha")
     private List<Restaurante> restaurantes = new ArrayList<Restaurante>();
-
 }
